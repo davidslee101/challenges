a2ee967a9632329a82c15fae393c7caedc19b660
david <davidslee101@gmail.com>
2015-07-20 22:11:02 +0000

    4th commit

--- array.rb
+++ array.rb
@@ -6,31 +6,40 @@ Row1=[1,2,3,4]
 Row2=[0,0,0,1]
 Row3=[2,3,4,5]
 Row4=[1,1,1,1]
-Row1.each {|x| print x}
+
+Row1.each {|x| print x} 
+puts "\n"
 Row2.each {|x| print x}
+puts "\n"
+Row3.each {|x| print x}
+puts "\n"
+Row4.each {|x| print x}
+puts "\n"
 
 
-class Image
-	attr_accessor :#, :#, :#, :#
+#class Image
+#	attr_accessor :#, :#, :#, :#
 
-	def initialize (#,#,#,#)
+#	def initialize (#,#,#,#)
 
-	end
+#	end
 
-	def output_image
+#	def output_image
+#		@#.each{|x| print x}
 
-	end
+#	end
 
-end
+#end
 
-image = Image.new ([
+#image = Image.new ([
+#	[0, 0, 0, 0],
+ # 	[0, 1, 0, 0],
+ #	[0, 0, 0, 1],
+ #	[0, 0, 0, 0]
+#	])
 
 
-
-	])
-
-
-image.output_image
+#image.output_image
 
 
 
